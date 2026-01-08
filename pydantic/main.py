e087a255cf3856b718938c95fb3f023e2ab4a7cb
faruq1905 <mdomar@krishworks.com>
2026-01-08 10:05:46 +0600

    Create product with validation and add a new calculated attribute

---
+++ pydantic/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Product(BaseModel):
+    id: int
+    name: str
+    price: float
+    stock: int
+
+## Create product with validation and add a new calculated attribute
+@app.post("/product")
+def create_product(new_product: Product):
+    product_dict = new_product.model_dump()
+    price_with_tax = new_product.price + (new_product.price * 15 / 100)
+    product_dict.update({"price_with_tax": price_with_tax})
+    return product_dict
